3e3e89d7a5aae1ceb023577dfea77eed61f3ecf7
Piet de Vries <p.h.devries@gmail.com>
2017-05-24 11:14:46 +0200

    start db implementation contract class

---
+++ app/src/main/java/com/rocdev/guardianreader/database/ArticleContract.java
@@ -0,0 +1,115 @@
+package com.rocdev.guardianreader.database;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by piet on 24-05-17.
+ *
+ */
+
+public class ArticleContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ArticleContract() {}
+
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.rocdev.guardianreader";
+
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.rocdev.inventoryTracker/articles/ is a valid path for
+     * looking at article data. content://com.rocdev.inventoryTracker/suppliers/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "suppliers".
+     */
+    public static final String PATH_ARTICLES = "articles";
+
+
+    public static final class ArticleEntry implements BaseColumns {
+
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_ARTICLES);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of articles.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ARTICLES;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single article.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ARTICLES;
+
+
+        /** Name of database table for articles */
+        public final static String TABLE_NAME = "articles";
+
+        /**
+         * Unique ID number for the article (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+
+
+        /**
+         * title of the article
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_ARTICLE_NAME = "title";
+
+
+        /**
+         * date of the article
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_ARTICLE_DATE = "date";
+
+
+        /**
+         * url of the article
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_ARTICLE_URL = "url";
+
+
+        /**
+         * url of the article
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_ARTICLE_SECTION = "section";
+
+
+        /**
+         * url of the article
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_ARTICLE_URL_THUMBNAIL = "thumbnail";
+
+    }
+
+}
