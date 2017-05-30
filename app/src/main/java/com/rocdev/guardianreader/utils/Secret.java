ff58e25e2a861f827cf527525d69eb19d1a15598
Piet de Vries <p.h.devries@gmail.com>
2017-05-30 09:24:12 +0200

    before merge v8

---
+++ app/src/main/java/com/rocdev/guardianreader/utils/Secret.java
@@ -0,0 +1,20 @@
+package com.rocdev.guardianreader.utils;
+
+/**
+ * Created by piet on 23-05-17.
+ *
+ */
+
+public class Secret {
+
+    private static final String API_KEY = "cc2f8fb0-4e6d-46f3-9588-2cdb8072943d";
+
+    /**
+     * private Constructor to avoid instanciating (i.e. static only)
+     */
+    private Secret() {}
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
