4460f86b70a4bd0c1508356f0d1638bc32293507
Piet de Vries <p.h.devries@gmail.com>
2017-06-25 13:14:55 +0200

    possible fix NullPointerException on ArticlesFragment

--- app/src/main/java/com/rocdev/guardianreader/fragments/ArticlesFragment.java
+++ app/src/main/java/com/rocdev/guardianreader/fragments/ArticlesFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -148,7 +147,7 @@ public class ArticlesFragment extends Fragment {
     }
 
     private void addNativeExpressAds() {
-        if (!articles.isEmpty()) {
+        if (articles != null && !articles.isEmpty()) {
             final NativeExpressAdView adView = new NativeExpressAdView(getContext());
             adView.setAdUnitId(getString(R.string.custom_small_ad_unit_id));
             final AdRequest.Builder builder = new AdRequest.Builder();
