95ac8c24b1a9a4ecb75e348b277444e0088818cd
Piet de Vries <p.h.devries@gmail.com>
2017-07-24 14:39:42 +0200

    change set Prefs summaries (Udacity)

--- app/src/main/java/com/rocdev/guardianreader/fragments/Settings2Fragment.java
+++ app/src/main/java/com/rocdev/guardianreader/fragments/Settings2Fragment.java
@@ -3,8 +3,13 @@ package com.rocdev.guardianreader.fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.SwitchPreferenceCompat;
 
 import com.rocdev.guardianreader.R;
 
@@ -16,60 +21,65 @@ import com.rocdev.guardianreader.R;
 public class Settings2Fragment extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private static final String PREF_KEY_DEFAULT_EDITION = "pref_default_edition";
-    private static final String PREF_KEY_DEFAULT_BROWSER = "pref_default_browser";
-
-
-    private ListPreference defaultEditionListPreference;
-    private ListPreference defaultBrowserListPreference;
-    private SharedPreferences mSharedPreferences;
-
-
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.preferences);
-        defaultEditionListPreference = (ListPreference) findPreference(
-                getString(R.string.pref_key_default_edition));
-        defaultBrowserListPreference = (ListPreference) findPreference(getString(R.string.pref_key_default_browser));
-        mSharedPreferences = getPreferenceManager().getSharedPreferences();
-        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
-        setDefaultBrowserSummary();
-        setDefaultEditionSummary();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
 
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+        int prefCount = preferenceScreen.getPreferenceCount();
+        for (int i = 0; i < prefCount; i++) {
+            Preference p = preferenceScreen.getPreference(i);
+            if (!(p instanceof SwitchPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    /**
+     * Updates the summary for the preference
+     *
+     * @param preference The preference to be updated
+     * @param value      The value that the preference was updated to
+     */
+    private void setPreferenceSummary(Preference preference, String value) {
+        // set summary of listpreferences
+        if (preference instanceof ListPreference) {
+            // For list preferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                // Set the summary to that label
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        mSharedPreferences = sharedPreferences;
-        switch (key) {
-            case PREF_KEY_DEFAULT_EDITION:
-                setDefaultEditionSummary();
-                break;
-            case PREF_KEY_DEFAULT_BROWSER:
-                setDefaultBrowserSummary();
-                break;
-            default:
-                // do nothing
+        Preference preference = findPreference(key);
+        if (preference != null) {
+            if (!(preference instanceof SwitchPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
         }
     }
 
-    private void setDefaultEditionSummary() {
-        int editionValue = Integer.parseInt(mSharedPreferences.getString(getString(
-                R.string.pref_key_default_edition), "3"));
-        String[] editionEntries = getResources().getStringArray(R.array.default_edition_entries);
-        defaultEditionListPreference.setSummary(editionEntries[editionValue]);
-    }
 
-    private void setDefaultBrowserSummary() {
-        int browserValue = Integer.parseInt(mSharedPreferences.getString(getString(
-                R.string.pref_key_default_browser),"0"));
-        String[] browserEntries = getResources().getStringArray(R.array.default_browser_entries);
-        defaultBrowserListPreference.setSummary(browserEntries[browserValue]);
-    }
 }
