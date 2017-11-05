b55a0c4a075d3bb9c83e15787fc2d03916a901fa
Piet de Vries <p.h.devries@gmail.com>
2017-11-05 09:17:34 +0100

    release ready v28

--- app/src/main/java/com/rocdev/guardianreader/activities/MainActivity.java
+++ app/src/main/java/com/rocdev/guardianreader/activities/MainActivity.java
@@ -59,9 +59,9 @@ public class MainActivity extends BaseActivity
      *******************************/
     private static final int CONTENT_CONTAINER = R.id.content_container;
     //TODO uncomment before building release-apk
-//    private static final String PARAM_VALUE_API_KEY = Secret.getApiKey();
+    private static final String PARAM_VALUE_API_KEY = Secret.getApiKey();
     //TODO comment out before building release-apk
-    private static final String PARAM_VALUE_API_KEY = "test";
+//    private static final String PARAM_VALUE_API_KEY = "test";
     private static final String KEY_ARTICLES = "articles";
     private static final String KEY_CURRENT_SECTION = "currentSection";
     private static final String KEY_CURRENT_PAGE = "currentPage";
@@ -329,8 +329,8 @@ public class MainActivity extends BaseActivity
         if (currentSection != Section.SAVED.ordinal()) {
 //            Bundle bundle = new Bundle();
 //            bundle.putString("section", titles[currentSection]);
-            //TODO comment in for production
-//            mFirebaseAnalytics.logEvent("api_call", null);
+            //TODO uncomment for production
+            mFirebaseAnalytics.logEvent("api_call", null);
         }
     }
 
